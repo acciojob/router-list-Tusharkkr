f15388f97e8ecff620b5b8b6cf99f4f6fb6a45da
Tushar kumar <narutoboi41@gmail.com>
2026-05-13 12:29:30 +0530

    dekhoo

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,15 @@ import React from "react";
 import './../styles/App.css';
 import ItemList from "./ItemList";
 import { Route, Routes } from "react-router-dom";
-import ItemDetail1 from "./ItemDetail1";
-import ItemDetail2 from "./ItemDetail2";
-import ItemDetail3 from "./ItemDetail3";
+import ItemDetail from "./ItemDetail1";
 
 const App = () => {
+
   return (
     <div>
         <Routes>
           <Route path="/" element={<ItemList/>}/>
-          <Route path="/Item/1" element={<ItemDetail1/>}/>
-          <Route path="/Item/2" element={<ItemDetail2/>}/>
-          <Route path="/Item/3" element={<ItemDetail3/>}/>
+          <Route path="/Item/:id" element={<ItemDetail/>}/>
         </Routes>
     </div>
   )
--- src/components/ItemDetail1.js
+++ src/components/ItemDetail1.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
-let ItemDetail1 = () => {
+let ItemDetail = () => {
+
+    let userData = [
+        { id: 1, name: 'Item 1', description: 'Description for Item 1' },
+        { id: 2, name: 'Item 2', description: 'Description for Item 2' },
+        { id: 3, name: 'Item 3', description: 'Description for Item 3' }
+    ]
+
+    let { id } = useParams()
+    let [data, setData] = useState(null)
+
+    useEffect(() => {
+        let value = userData.find(value => value.id == id)
+        setData(value)
+    }, [])
 
     return (
         <div>
-            <h1>Item 1</h1>
-            <p>Description for Item 1</p>
+            {/* {data && <div>
+                <h1>{data.name}</h1>
+                <p>{data.description}</p>
+            </div>} */}
         </div>
     )
 }
 
-export default ItemDetail1
+export default ItemDetail
